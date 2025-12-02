5e447791816877ccf340c93af7c5b234668b8ffb
vishnuarumugam <vishnuarumugam95@gmail.com>
2025-12-02 14:12:50 +0530

    ADDED:: Assets

---
+++ src/assets/logos/index.tsx
@@ -0,0 +1,15 @@
+import ESDSLogo from "./esds-bg-image.png";
+import BackgroundAuth from "./background_auth.png";
+import BackgroundAuthRobot from "./background_auth_robot.png";
+import AuthLogoBadge from "./ESDS-Logo-Badge.png";
+
+const LOGO_IMAGES_PATH = {
+    ESDSLogo,
+    BackgroundAuth,
+    BackgroundAuthRobot,
+    AuthLogoBadge
+};
+
+Object.freeze(LOGO_IMAGES_PATH);
+
+export { LOGO_IMAGES_PATH };
