58eee41ff4004873a20bddbd9e390cd5206433fd
vishnuarumugam <vishnuarumugam95@gmail.com>
2025-12-02 14:02:53 +0530

    ADDED:: String

---
+++ src/i18n/de/strings.ts
@@ -0,0 +1,4 @@
+export default {
+    noAccountYet: 'Noch kein Konto?',
+    resetPassword: 'Passwort zurücksetzen',
+}
---
+++ src/i18n/en/strings.ts
@@ -0,0 +1,5 @@
+export default {
+    noAccountYet: 'Do not have an account yet?',
+    resetPassword: 'Reset password',
+    doLogIn: 'Single Sign-On',
+}
